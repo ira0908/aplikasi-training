254e8dee32c77f4e7f811e77ba8e560e429bbc1a
ira <ira@dwirahmah.(none)>
2011-12-14 14:48:55 +0700

    hapus semua

--- system/save2.php
+++
@@ -1,8 +0,0 @@
-<?php
-	include ("../configuration/configuration.php");
-	
-	$kode	=$_POST["kode_paket_training"];
-	$nama	=$_POST["nama_paket_training"];
-	
-	$save	= mysql_query("insert into Paket_Training (kode_paket_training,nama_paket_training)values($kode,'$nama')");
-?>
