85e4759887f71a2bc60e3f0359cf50cd78632dc9
opaslowdance <opaslowdance@engineer.com>
2011-12-15 16:13:32 +0700

    form login

---
+++ form/login.php
@@ -0,0 +1,43 @@
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="../css/design.css" />
+		<title>
+			Sign In
+		</title>
+		<body>
+			<form method="post" action="../system/login.php">
+				<table align="center" cellpadding="0" cellspacing="0">
+					<tr class="top">
+						<th colspan="2" class="top">
+							Sign In
+						</th>
+					</tr>
+					<tr class="mid">
+						<td colspan="2" class="mid">
+							<input type="text" placeholder="Username" name="username" class="input" />
+						</td>
+					</tr>
+					<tr class="mid">
+						<td colspan="2" class="mid">
+							<input type="password" placeholder="Password" name="password" class="input" />
+						</td>
+					</tr>
+					<tr class="bottom">
+						<td class="left">
+							<a href="#">
+								Sign Up
+							</a>
+							|
+							<a href="#">
+								Forgot Password ?
+							</a>
+						</td class="right">
+						<td>
+							<input type="submit" value="Sign In" name="submit" class="button" />
+						</td>
+					</tr>
+				</table>
+			</form>
+		</body>
+	</head>
+</html>
